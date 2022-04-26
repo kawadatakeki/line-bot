28022b2383a296f5a1b682d6719b05321ed9e494
kawadatakeki <kawada.takeki424@mail.kyutech.jp>
2022-04-26 19:27:39 +0900

    Update: echo bot

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
+const line = require("@line/bot-sdk");
+require("dotenv").config();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const config = {
+  channelAccessToken: process.env.LINE_MESSAGING_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_MESSAGING_CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+
+exports.lineBot = functions
+  .region("asia-northeast1")
+  .https.onRequest(async (request, response) => {
+    const event = request.body.events[0];
+    const text = event.message.text;
+    const textMessage = {
+      type: "text",
+      text: text,
+    };
+    const result = await client.replyMessage(event.replyToken, textMessage);
+    response.json(result);
+  });
